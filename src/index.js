f42d91280836314c0cd163fb986c8c4cefe0595a
Almaz <63015341+Morr231@users.noreply.github.com>
2022-06-17 16:09:11 +0600

    moralis boilerplate

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
+import { MoralisProvider } from "react-moralis";
+import "./index.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import QuickStart from "components/QuickStart";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+/** Get your free Moralis Account https://moralis.io/ */
+
+const APP_ID = "NmqTCwvcHeC4w38zcby8HjTkz7M9hcrKxrF7DzYL";
+const SERVER_URL = "https://uyr8msajsk5w.usemoralis.com:2053/server";
+
+const Application = () => {
+    const isServerInfo = APP_ID && SERVER_URL ? true : false;
+    //Validate
+    if (!APP_ID || !SERVER_URL)
+        throw new Error(
+            "Missing Moralis Application ID or Server URL. Make sure to set your .env file."
+        );
+    if (isServerInfo)
+        return (
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+                <App isServerInfo />
+            </MoralisProvider>
+        );
+    else {
+        return (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <QuickStart />
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(
+    <StrictMode>
+        <Application />
+    </StrictMode>,
+    document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
